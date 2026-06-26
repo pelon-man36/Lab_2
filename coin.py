f7591c6dce0d81d9bffac8e103c21616eb478422
Johan Ramirez <johand200505@gmail.com>
2026-06-26 16:11:19 -0400

    Added if statment to decide _sideup as either Heads or Tails

--- coin.py
+++ coin.py
@@ -6,3 +6,8 @@ class Coin():
 
     def toss(self):
         flip = randint(0, 1)
+        if flip == 0:
+            self._sideup = "Heads"
+        else:
+            self._sideup = "Tails"
+        print(self._sideup)
