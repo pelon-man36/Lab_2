e6cb20351ac2206d1d720c55eec492ecffa229aa
Johan Ramirez <johand200505@gmail.com>
2026-06-27 13:50:34 -0400

    Created class Player and method __init__ for player.py

--- player.py
+++ player.py
@@ -1,1 +1,9 @@
 from coin import Coin
+
+class Player(Coin):
+    def __init__(self, _name="Player 1", _wallet=20, _coin=Coin()):
+        self._name = _name
+        self._wallet = _wallet
+        self._coin = _coin
+
+
