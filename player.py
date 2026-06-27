9e3ca59df06305830a734afd7d8bf19fe1f631c3
Johan Ramirez <johand200505@gmail.com>
2026-06-27 14:49:03 -0400

    Created class Player(Coin) and method toss_coin

--- player.py
+++ player.py
@@ -1,7 +1,9 @@
 from coin import Coin
+class Player(Coin):
+    def __init__(self, _name="Player 1", _coin=Coin(),  _wallet=20):
+        self._name = _name
+        self._name = _wallet
+        self._coin = _coin
 
-def __init__(self, _name, _wallet, _coin):
-    self._name = _name
-    self._name = _wallet
-    self._coin = _coin
-
+    def toss_coin(self):
+        
