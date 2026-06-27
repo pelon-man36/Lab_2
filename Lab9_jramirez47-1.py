e1c70bf8bb73538066c105785d8b023ddaf36d59
Johan Ramirez <johand200505@gmail.com>
2026-06-27 17:31:28 -0400

    Added print statement that shows how many coins each player has

--- Lab9_jramirez47-1.py
+++ Lab9_jramirez47-1.py
@@ -3,6 +3,8 @@ from player import Player
 def main():
     player1 = Player("Player 1")
     player2 = Player("Player 2")
+    print(f"{player1.get_name()} has {player1.get_wallet()} coins.")
+    print(f"{player2.get_name()} has {player2.get_wallet()} coins.")
     option = input("Do you want to toss a coin?(y/n) ")
 
     while option != "n":
@@ -22,7 +24,9 @@ def main():
             print(f"No match! {player2.get_name()} wins a coin!")
             player1.lose_coin()
             player2.win_coin()
-        
+            
+        print(f"{player1.get_name()} has {player1.get_wallet()} coins.")
+        print(f"{player2.get_name()} has {player2.get_wallet()} coins.")
         option = input("Do you want to toss a coin?(y/n) ")
 
 main()
