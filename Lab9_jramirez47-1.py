bbce27cf740bd5f623e9b66e6df608040d16c5a2
Johan Ramirez <johand200505@gmail.com>
2026-06-27 17:20:14 -0400

    Fixed issue where it gave either double heads or tails, never heads and tails/tails and heads

--- Lab9_jramirez47-1.py
+++ Lab9_jramirez47-1.py
@@ -6,7 +6,13 @@ def main():
     option = input("Do you want to toss a coin?(y/n) ")
 
     while option != "n":
-        print("Hi")
+        print("Tossing coins...")
+        player1.toss_coin()
+        side1 = player1.get_coin_side()
+        player2.toss_coin()
+        side2 = player2.get_coin_side()
+        print(f"{player1.get_name()} got {side1}.")
+        print(f"{player2.get_name()} got {side2}.")
         option = input("Do you want to toss a coin?(y/n) ")
 
 main()
