f836b3cb9de4cb5901b446cff1ba8fd860e46efb
Johan Ramirez <johand200505@gmail.com>
2026-06-27 14:54:03 -0400

    Added method get_coin_side

--- player.py
+++ player.py
@@ -1,9 +1,13 @@
 from coin import Coin
 class Player(Coin):
-    def __init__(self, _name="Player 1", _coin=Coin(),  _wallet=20):
+    def __init__(self, _name="Player 1", _wallet=20, _coin=Coin()):
         self._name = _name
         self._name = _wallet
         self._coin = _coin
 
     def toss_coin(self):
-        
+        self._coin.toss()
+
+    def get_coin_side(self):
+        get = Coin()
+        get.get_sideup()
