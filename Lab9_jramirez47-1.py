a3561776d48242170bca3eaa23a61bd9c3bf6c91
Johan Ramirez <johand200505@gmail.com>
2026-06-27 15:30:23 -0400

    Created user input and while loop for game

--- Lab9_jramirez47-1.py
+++ Lab9_jramirez47-1.py
@@ -3,4 +3,7 @@ from player import Player
 def main():
     player1 = Player("Player 1")
     player2 = Player("Player 2")
+    option = input("Do you want to toss coins? (y/n) ")
     
+    while option != "n":
+        
